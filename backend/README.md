3bcbed0dac0bcafcf560821d010f58a70655d946
Anjali Saini <anjalisaini0192@gmail.com>
2026-03-09 10:08:13 +0530

    Convert backend and frontend from submodules to normal directories and remove env file

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+# career-connect
+A full stack professional networking platform inspired by LinkedIn built with Next.js and Node.js
---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import postRoutes from './routes/posts.routes.js';
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(postRoutes);
+
+app.use(cors());
+
+app.use(express.json());
+
+const start = async () => {
+  
+    const connectDB = await mongoose.connect("mongodb+srv://anjalisaini0192_db_user:y1DOgfeuRpPs4gBr@apnaproconnect.zqireqv.mongodb.net/?appName=apnaproconnect");
+
+    app.listen(9080, () => {
+        console.log("Server is running on port 9080");
+    })
+}
+
+start();
