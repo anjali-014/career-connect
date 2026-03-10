c3d4943ec35e8be83d811e541f8766ae6cebbb22
Anjali Saini <anjalisaini0192@gmail.com>
2026-03-10 20:28:37 +0530

    Implement user registration controller with password hashing

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -1,5 +1,48 @@
+import User from "../models/user.model.js";
+import Profile from "../models/profiles.model.js";
 
+import bcrypt from "bcrypt";
 
  export const activeCheck = async (req, res) => {
+
     return res.status(200).json({message: "Active"});
+
+}
+
+const register = async (req, res) => {
+    try{
+        const {name, email, password, username} = req.body;
+        if(!name || !email || !password || !username){
+            return res.status(400).json({message: "All fields are required"});
+        }
+            const user = await User.findOne({
+                email
+            });
+            if(user){
+                return res.status(400).json({message: "User already exists"});
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+            const newUser = new User({
+                name,
+                email,
+                password,
+                username
+            });
+            await newUser.save();
+
+            const profile = new Profile({userId: newUser._id });
+
+            await profile.save();
+
+            return res.status(201).json({message: "User created successfully", user: newUser});
+
+    
+
+
+    }
+
+    catch (error){
+        return res.status(500).json({message: error.message});
+    }
 }
