f553ebf0934c0ebb966a5507a33beaa6cbb3ffd8
Anjali Saini <anjalisaini0192@gmail.com>
2026-03-20 22:08:56 +0530

    updated user routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,8 +1,29 @@
 import { Router} from "express";
-import { register } from "../controllers/user.controller.js";
+import { login, register } from "../controllers/user.controller.js";
+import multer from "multer";
 
 const router = Router();
 
+const storage = multer.diskStorage({
+    destination : (req,file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+
+const upload = multer({storage: storage});
+
+//updated user routes
+router.route("/update_profile_picture")
+.post(upload.single("profile_picture"), uploadProfilePicture);
+
+
+
 router.route("/register").post(register);
+
+router.route("/login").post(login);
 
 export default router;
