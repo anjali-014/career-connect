62c6c9efb81f310d55b7fe76f764a1259337c2ce
Anjali Saini <anjalisaini0192@gmail.com>
2026-03-23 08:29:59 +0530

    Integrate PDF feature with routes and server

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router} from "express";
-import { login, register } from "../controllers/user.controller.js";
+import { getAllUserProfiles, login, register } from "../controllers/user.controller.js";
 import multer from "multer";
 import { uploadProfilePicture } from "../controllers/user.controller.js";
 import {updateUserProfile} from "../controllers/user.controller.js";
 import {getUserAndProfile} from "../controllers/user.controller.js";
 import {updateProfileData} from "../controllers/user.controller.js";
+import {downloadProfile}  from "../controllers/user.controller.js";
+// import {getAllUserProfiles}  from "../controllers/user.controller.js";
 
 const router = Router();
 
@@ -35,5 +37,10 @@ router.route("/user_update").post(updateUserProfile);
 router.route("/get_user_and_profile").get(getUserAndProfile);
 
 router.route("/update_profile_data").post(updateProfileData);
+
+router.route("/get_all_users").get(getAllUserProfiles);
+
+router.route("/download_resume").get(downloadProfile);
+
 
 export default router;
