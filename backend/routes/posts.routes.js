f1bd83add9491569fc2363c7602070546c82552a
Anjali Saini <anjalisaini0192@gmail.com>
2026-03-24 17:02:10 +0530

    Add connection request routes in post routes file

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,7 +1,20 @@
 import { Router} from "express";
 import { activeCheck } from "../controllers/posts.controller.js";
+import multer from "multer";
 
 const router = Router();
+
+const storage = multer.diskStorage({
+    destination : (req,file, cb) => {
+        cb(null, 'uploads/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({storage: storage});
+
 
 router.route("/").get(activeCheck);
 
