501102dccfb334f1caaac7eda92367e650f359f8
Anjali Saini <anjalisaini0192@gmail.com>
2026-04-01 23:09:27 +0530

    Set up Redux store configuration for state management

---
+++ frontend/src/config/redux/store.js
@@ -0,0 +1,13 @@
+ import { configureStore } from "@reduxjs/toolkit";
+//  STEPS FOR STATE MANAGEMENT...
+
+//  * Submit an action
+//  * Handle the action in a reducer
+//  * Register here -> reducer
+
+
+const store = configureStore({
+
+})
+
+export default store;
