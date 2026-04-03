51742281b14e933775163a8d7ab2322be48a7707
Anjali Saini <anjalisaini0192@gmail.com>
2026-04-03 22:03:35 +0530

    feat(auth): implement auth slice with login state handling and async reducers

---
+++ frontend/src/config/redux/reducer/authReducer/index.js
@@ -0,0 +1,57 @@
+const { createSlice } = require("@reduxjs/toolkit");
+const { loginUser } = require("../action/authAction");
+
+
+const initialState = {
+    user : [],
+    isError : false,
+    isSuccess : false,
+    isLoading : false,
+    loggedIn : false,
+    message : "",
+    profileFetched : false,
+    connections : [],
+    connetionRequests : [],
+}
+
+
+const authSlice = createSlice({
+    name : "auth",
+    initialState,
+    reducers : {
+        reset : () => initialState,
+        handleLoginUser : (state) => {
+            state.message = "hello";
+        }
+    },
+    extraReducers : (builder) => { 
+
+        builder
+
+        .addCase(loginUser.pending, (state) => {
+            state.isLoading = true;
+            state.message = " Knocking the door...";
+        })
+
+        .addCase(loginUser.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.isSuccess = true;
+            state.loggedIn = true;
+            state.isError = false;
+            state.message = "Welcome to LinkedIn!";
+
+        })
+
+        .addCase(loginUser.rejected, (state, action) => {
+            state.isLoading = false;
+            state.isError = true;
+            state.isSuccess = false;
+            state.loggedIn = false;
+            state.message = action.payload.message || "Login failed. Please try again.";
+        })
+
+    }
+
+});
+
+export default authSlice.reducer;
