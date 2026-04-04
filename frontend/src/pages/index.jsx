fd9e3e61fd0ca5db9627d9a8855a6e876ed32552
Anjali Saini <anjalisaini0192@gmail.com>
2026-04-04 23:34:24 +0530

    feat(ui): update home page with welcome heading

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,17 @@
+// modified index.js
+
+//import {inter} from "next/font/google";
+
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+//const inter = Inter({ subsets: ["latin"] });
+
+
+export default function Home() {
+  return (
+    <>
+        <h2>Welcome to LinkedIn</h2>
+    </>
+  );
+}
