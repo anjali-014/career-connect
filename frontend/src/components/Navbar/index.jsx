50dcd39fe0c684f0abed672cdf3548953442e540
Anjali Saini <anjalisaini0192@gmail.com>
2026-04-07 21:54:17 +0530

    feat(ui): add basic NavBar component structure with styling

---
+++ frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from "./styles.module.css";
+
+function NavBarComponent() {
+  return (
+    <div className={styles.Container}>
+       <nav className={styles.Navbar}>
+
+        <h2>Pro Connect</h2>
+
+        <div className>
+
+
+        </div>
+
+       
+       </nav>
+      
+    </div>
+  )
+}
+
+export default NavBarComponent;
