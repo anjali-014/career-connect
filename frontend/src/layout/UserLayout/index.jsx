f7a24393cb93b0990b75adb76a855548dfc80260
Anjali Saini <anjalisaini0192@gmail.com>
2026-05-21 22:15:09 +0530

    Updated Home Module CSS

---
+++ frontend/src/layout/UserLayout/index.jsx
@@ -0,0 +1,15 @@
+import NavBarComponent from '@/components/Navbar';
+import React from 'react'
+     
+
+ function UserLayout({ children }) {
+  return (
+    <div>
+
+        <NavBarComponent/>
+      {children}
+    </div>
+  )
+}
+
+export default UserLayout;
---
+++ frontend/src/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+ function LoginComponent() {
+  return (
+    <div>
+      <div>Login Component</div>
+    </div>
+  )
+}
+
+export default LoginComponent;
+
