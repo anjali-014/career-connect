6d821fcdea84a222ea3e7ff5b2692d8e9f065a55
Anjali Saini <anjalisaini0192@gmail.com>
2026-05-27 19:07:36 +0530

    Update frontend configuration setup

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,5 @@
-const { default : axios} = require("axios");
-
+import axios from "axios";
 
 export const clientServer = axios.create({
-    baseURL : "http://localhost:9090",
-
-})
+  baseURL: "http://localhost:9090",
+});
